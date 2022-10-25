d25b32f7d0e5f1954bbc3bc4616b4565d157390d
Mazedul Hasan <mazdul1000@gmail.com>
2022-10-25 11:47:24 +0600

    Add files via upload

---
+++ demo-todo/src/components/TodoList.js
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTodos } from "../redux/todos/thunk/fetchTodos";
+import Todo from "./Todo";
+
+
+const TodoList = () => {
+    const todos = useSelector((state) => state.todos);
+    const filters = useSelector((state) => state.filters);
+    const disptach = useDispatch()
+
+    useEffect(() => {
+       disptach(fetchTodos)  
+    },[])
+
+    const filterByStatus = (todo) => {
+        const {status} = filters;
+        switch (status) {
+            case 'Complete':
+                return todo.completed
+                
+            case 'Incomplete':
+                return !todo.completed
+        
+            default:
+                return true;
+        }
+     }
+    
+     const filterByColors = (todo) => {
+        const {colors} = filters;
+        if(colors.length > 0){
+         return colors.includes(todo?.color);
+        }
+        return true;
+     }
+
+    return (
+        <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+         {todos
+         .filter(filterByStatus)
+         .filter(filterByColors)
+         .map(todo => <Todo key={todo.id} todo={todo}/>)}   
+        </div>
+    );
+}
+
+export default TodoList;
